c830c5869ad15a0aafd1cb1bf109acfd52b89997
Shirlette Chambers <shirlette.chambers@gmail.com>
2017-01-26 11:14:39 -0500

    New requests to the server addded

--- routes/routes.js
+++ routes/routes.js
@@ -31,43 +31,56 @@ module.exports = function(){
         // Response JSON object
         reply({'user':user, 'responseCode':0});
       }
-  }
-  ]
-}();
+  },
 
-// server.route({ 
-// method: 'POST', 
-// path: '/api/user', 
-// config: { // "tags" enable swagger to document API 
-// 	tags: ['api'], 
-// 	description: 'Save user data', 
-// 	notes: 'Save user data', 
-// 	// We use Joi plugin to validate request 
-// 	validate: { 
-// 		payload: { 
-// 		// Both name and age are required fields 
-// 		name: Joi.string().required(), 
-// 		age: Joi.number().required() 
-// 		} 
-// 	} 
-// }, handler: function (request, reply) { 
-// 	// Create mongodb user object to save it into database 
-// 	var user = new UserModel(request.payload); 
-// 	// Call save methods to save data into database 
-// 	// and pass callback methods to handle error 
-// 	user.save(function (error) { 
-// 		if (error) {
-// 		 reply({ 
-// 		 	statusCode: 503, 
-// 		 	message: error 
-// 		 }); 
-// 		} 
-// 		else {
-// 		 reply({ 
-// 		 	statusCode: 201, 
-// 		 	message: 'User Saved Successfully' 
-// 		 }); 
-// 		} 
-// 	}); 
-// } 
-// }); 
+  {
+    method: 'GET',
+    path: '/user/{id}',
+    config: {
+      handler: function(request, reply){
+        if (store.length <= request.params.id){
+          return reply({message:"user does not exist", responseCode: 1}).code(404);
+        }
+        reply({'user':user[request.params.id], 'responseCode':0})
+      }
+    }
+  },
+  
+{ 
+method: 'POST', 
+path: '/api/user', 
+config: { // "tags" enable swagger to document API 
+	tags: ['api'], 
+	description: 'Save user data', 
+	notes: 'Save user data', 
+	// We use Joi plugin to validate request 
+	validate: { 
+		payload: { 
+		// Both name and age are required fields 
+		name: Joi.string().required(), 
+		age: Joi.number().required() 
+		} 
+	} 
+}, handler: function (request, reply) { 
+	// Create mongodb user object to save it into database 
+	var user = new UserModel(request.payload); 
+	// Call save methods to save data into database 
+	// and pass callback methods to handle error 
+	user.save(function (error) { 
+		if (error) {
+		 reply({ 
+		 	statusCode: 503, 
+		 	message: error 
+		 }); 
+		} 
+		else {
+		 reply({ 
+		 	statusCode: 201, 
+		 	message: 'User Saved Successfully' 
+		 }); 
+		} 
+	}); 
+} 
+}
+];
+}();
