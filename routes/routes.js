f940e64e5ea1f91af6f951935640ca697a1c005f
Shirlette Chambers <shirlette.chambers@gmail.com>
2017-01-26 11:45:34 -0500

    Post request of new user

--- routes/routes.js
+++ routes/routes.js
@@ -16,8 +16,10 @@ module.exports = function(){
     }
   ]
   return [
-    // ======================== Routes for the API ==================
-      // Define GET Routes
+// ======================== Routes for the API (in an array) ==================
+// Define GET Routes
+
+//Get user data
       {
       method: 'GET',  //method type
       path: '/user/', //Url
@@ -33,6 +35,7 @@ module.exports = function(){
       }
   },
 
+//Get User ID ( if available)
   {
     method: 'GET',
     path: '/user/{id}',
@@ -45,42 +48,73 @@ module.exports = function(){
       }
     }
   },
-  
-{ 
-method: 'POST', 
-path: '/api/user', 
-config: { // "tags" enable swagger to document API 
-	tags: ['api'], 
-	description: 'Save user data', 
-	notes: 'Save user data', 
-	// We use Joi plugin to validate request 
-	validate: { 
-		payload: { 
-		// Both name and age are required fields 
-		name: Joi.string().required(), 
-		age: Joi.number().required() 
-		} 
-	} 
-}, handler: function (request, reply) { 
-	// Create mongodb user object to save it into database 
-	var user = new UserModel(request.payload); 
-	// Call save methods to save data into database 
-	// and pass callback methods to handle error 
-	user.save(function (error) { 
-		if (error) {
-		 reply({ 
-		 	statusCode: 503, 
-		 	message: error 
-		 }); 
-		} 
-		else {
-		 reply({ 
-		 	statusCode: 201, 
-		 	message: 'User Saved Successfully' 
-		 }); 
-		} 
-	}); 
-} 
-}
-];
+
+//Post new user into dataset
+  {
+    method: 'POST',
+    path: '/user/',
+    config: {
+      handler: function(request,reply){
+        console.log(request.payload);
+        user.push({
+          name: request.payload.name,
+          age: request.payload.age
+        });
+        reply({message:'Successfully added the new user', responseCode:0});
+      }
+    }
+  },
+
+  {
+    method: 'POST',
+    path: '/user',
+    config: {
+      handler: function(request, reply){
+        console.log(request.payload);
+        user.push({
+          name: request.payload.name,
+          age: request.payload.age
+        });
+        reply({message:'Successfully added the new user', responseCode:0});
+      }
+    }
+  }
+  ];
 }();
+
+// server.route({ 
+// method: 'POST', 
+// path: '/api/user', 
+// config: { // "tags" enable swagger to document API 
+// 	tags: ['api'], 
+// 	description: 'Save user data', 
+// 	notes: 'Save user data', 
+// 	// We use Joi plugin to validate request 
+// 	validate: { 
+// 		payload: { 
+// 		// Both name and age are required fields 
+// 		name: Joi.string().required(), 
+// 		age: Joi.number().required() 
+// 		} 
+// 	} 
+// }, handler: function (request, reply) { 
+// 	// Create mongodb user object to save it into database 
+// 	var user = new UserModel(request.payload); 
+// 	// Call save methods to save data into database 
+// 	// and pass callback methods to handle error 
+// 	user.save(function (error) { 
+// 		if (error) {
+// 		 reply({ 
+// 		 	statusCode: 503, 
+// 		 	message: error 
+// 		 }); 
+// 		} 
+// 		else {
+// 		 reply({ 
+// 		 	statusCode: 201, 
+// 		 	message: 'User Saved Successfully' 
+// 		 }); 
+// 		} 
+// 	}); 
+// } 
+// }); 
